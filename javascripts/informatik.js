2fb05291eaf58d671b5956099b82f99f777af290
Martin Linhard <linmad17@htl-kaindorf.at>
2019-03-10 19:05:23 +0100

    First version of search bar

---
+++ javascripts/informatik.js
@@ -0,0 +1,7 @@
+particlesJS.load('particles-js', 'javascripts/assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+function showDropDown(){
+  alert("i bims");
+}
